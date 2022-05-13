9d9b3d932abfb84452644d8fa59d37d5b7888964
Michael Willett <mikerwillett@gmail.com>
2022-05-13 14:16:46 -0500

    got vscode building and running inside /blindr. NOTE: the /src dir must be opened in vscode, not the entire /blindr dir

---
+++ src/subdir/blink.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+int main() {
+#ifndef PICO_DEFAULT_LED_PIN
+#warning blink example requires a board with a regular LED
+#else
+    const uint LED = PICO_DEFAULT_LED_PIN;
+    stdio_init_all();
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
+    while (true) {
+        printf("Hello, world!\n");
+        gpio_put(LED, 1);
+        sleep_ms(500);
+        gpio_put(LED, 0);
+        sleep_ms(500);
+    }
+    return 0;
+#endif
+}
