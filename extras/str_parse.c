e6b2e8abc0972db64e8c928f47b5bad554e8dc48
Michael Willett <mikerwillett@gmail.com>
2022-05-20 00:50:24 -0500

    small loop to interate through sentences and print all valid ones. this can be used to further parse useful sentences such as GGA or VTG identifiers

--- extras/str_parse.c
+++ extras/str_parse.c
@@ -4,15 +4,6 @@
 int parse_comma_delimited_str(char *string, char **fields, int max_fields);
 void parse_buffer(char *buffer, char **sentences);
 
-int parse_comma_delimited_str(char *string, char **fields, int max_fields) {
-   int i = 0;
-   fields[i++] = string;
-   while ((i < max_fields) && NULL != (string = strchr(string, ','))) {
-      *string = '\0';
-      fields[i++] = ++string;
-   }
-   return --i;
-}
 
 void parse_buffer(char *buffer, char **sentences) {
     /*
@@ -33,7 +24,7 @@ int parse_line(char *buffer, char **fields, int max_fields) {
 	fields[i++] = buffer;
 
 	while ((i < max_fields) && NULL != (buffer = strchr(buffer, ','))) {
-        if (buffer[0] == '$') {
+        if (buffer[0] == '$') {  // is this needed now that lines are split by `\r\n` ???
             printf("breaking out\n");
             break;
         }
@@ -43,20 +34,37 @@ int parse_line(char *buffer, char **fields, int max_fields) {
 	return --i;
 }
 
+int parse_comma_delimited_str(char *string, char **fields, int max_fields) {
+   int i = 0;
+   fields[i++] = string;
+   while ((i < max_fields) && NULL != (string = strchr(string, ','))) {
+      *string = '\0';
+      fields[i++] = ++string;
+   }
+   return --i;
+}
+
 
 int main() {
 	char buffer[] = "$GNGSA,4554,2322,1111,5555\r\n$GNGPA,aa,bb,cc,dd,ee,ff\r\n$GARMC,q1,q2,q3,q4,q5,q6\r\n";  // simulated NMEA data
 	char *sentences[8];  // array of pointers pointing to the location of the start of each sentence
-	char **fields[20];  // location of each field of each sentence
+	// char **fields[20];  // location of each field of each sentence
 	// char line[] = "$GNSS,4554,2322,1111,5555";
 	char *field[20];
-	parse_buffer(buffer, sentences);  // split the monolithic buffer into discrete sentences
-	printf("1: %s\n\n", sentences[0]);
-	printf("2: %s\n\n", sentences[1]);
-	printf("3: %s\n\n", sentences[2]);
-	printf("4: %s\n\n", sentences[3]);
 	
-	parse_comma_delimited_str(sentences[0], field, 20);
+	parse_buffer(buffer, sentences);  // split the monolithic buffer into discrete sentences
+
+	// printf("1: %s\n\n", sentences[0]);
+	// printf("2: %s\n\n", sentences[1]);
+	// printf("3: %s\n\n", sentences[2]);
+	// printf("4: %s\n\n", sentences[3]);
+
+	int i = 0;
+	while (sentences[i] != NULL) {
+		printf("loop: %s\n", sentences[i++]);
+	}
+	
+	// parse_comma_delimited_str(sentences[0], field, 20);
 	// printf("%s\n", field[0]);
 	// printf("%s\n", field[1]);
 	// printf("%s\n", field[2]);
