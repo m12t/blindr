696539f91319da40d53e85c089c59899bb6dbcbd
Michael Willett <mikerwillett@gmail.com>
2022-05-22 17:28:15 -0500

    successfully set the onboard pico RTC

--- src/setup/setup.c
+++ src/setup/setup.c
@@ -3,3 +3,33 @@
 #include "pico/util/datetime.h"
 #include "hardware/rtc.h"
 
+// setup the RTC on pico
+
+int main(void) {
+    stdio_init_all();
+    printf("init`ng RTC\n");
+
+    char datetime_buffer[256];
+    char *datetime_str = &datetime_buffer[0];  // address of first element in buffer
+
+    datetime_t dt = {
+        .year = 2022,
+        .month = 05,
+        .day = 22,
+        .dotw = 0,
+        .hour = 17,
+        .min = 45,
+        .sec = 00
+    };
+
+    rtc_init();  // initialize the onboard RTC
+    rtc_set_datetime(&dt);
+
+    while (1) {
+        rtc_get_datetime(&dt);
+        datetime_to_str(datetime_str, sizeof(datetime_buffer), &dt);
+        printf("\r%s\t", datetime_str);
+        sleep_ms(100);
+    }
+    return 0;
+}
