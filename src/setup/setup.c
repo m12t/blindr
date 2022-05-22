1e4b6f3f05d2bfda4b86411f01599a3bb428910a
Michael Willett <mikerwillett@gmail.com>
2022-05-22 17:41:03 -0500

    removed main() from setup.c and finished the set_onboard_rtc() function to take parsed ZDA date time data and set the RTC correspondingly

--- src/setup/setup.c
+++ src/setup/setup.c
@@ -3,40 +3,20 @@
 #include "pico/util/datetime.h"
 #include "hardware/rtc.h"
 
-// setup the RTC on pico
 
+void set_onboard_rtc(int year, int month, int day, int hour, int min, int sec) {
+    /* receive parsed ZDA data and populate set the RTC to the current time */
 
-int set_onboard_rtc() {
-    // receive ZDA data (parsed or not?) and populate set the RTC to the current time.
-
-}
-
-
-int main(void) {
-    stdio_init_all();
-    printf("init`ng RTC\n");
-
-    // char datetime_buffer[256];
-    // char *datetime_str = &datetime_buffer[0];  // address of first element in buffer
-
+    // construct the datetime_t struct and populte with the parameters data
     datetime_t dt = {
-        .year = 2022,
-        .month = 05,
-        .day = 22,
-        .dotw = 0,
-        .hour = 17,
-        .min = 45,
-        .sec = 00
+        .year = year,
+        .month = month,
+        .day = day,
+        .hour = hour,
+        .min = min,
+        .sec = sec,
     };
 
-    rtc_init();  // initialize the onboard RTC
+    rtc_init();
     rtc_set_datetime(&dt);
-
-    while (1) {
-        rtc_get_datetime(&dt);
-        // datetime_to_str(datetime_str, sizeof(datetime_buffer), &dt);
-        printf("\rsec: %d\t", dt.sec);
-        sleep_ms(100);
-    }
-    return 0;
 }
