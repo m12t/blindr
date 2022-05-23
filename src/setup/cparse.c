f2c4e91e89a5f2f187b10b04e44a521a12fc6f73
Michael Willett <mikerwillett@gmail.com>
2022-05-22 19:20:07 -0500

    refactored cparse to parse all values in one function and changed types to match those expected by the datetime_t struct

--- src/setup/cparse.c
+++ src/setup/cparse.c
@@ -11,8 +11,13 @@
 // 5: 00 (Local time zone hours; always 00)
 // 6: 00 (Local time zone minutes; always 00)
 
-void parse_time(char *time, int *hour, int *min, int *sec) {
+void parse_time(char **zda_msg, int16_t *year, int8_t *month, int8_t *day,
+                int8_t *hour, int8_t *min, int8_t *sec) {
     // convert the char to int and formulate the int using tens and ones places.
+    *year = atoi(zda_msg[4]);
+    *month = atoi(zda_msg[3]);
+    *day = atoi(zda_msg[2]);
+    char *time = zda_msg[1];
     *hour = 10 * (time[0] - '0') + (time[1] - '0');
     *min = 10 * (time[2] - '0') + (time[3] - '0');
     *sec = 10 * (time[4] - '0') + (time[5] - '0');
@@ -21,21 +26,22 @@ void parse_time(char *time, int *hour, int *min, int *sec) {
 int main(void) {
     char *zda_msg[7];
     zda_msg[0] = "$GNZDA";
-    zda_msg[1] = "220929.00";
+    zda_msg[1] = "191910.00";
     zda_msg[2] = "21";
     zda_msg[3] = "05";
     zda_msg[4] = "2022";
     zda_msg[5] = "0";
     zda_msg[6] = "0";
 
-    int hour, min, sec;
-    parse_time(zda_msg[1], &hour, &min, &sec);
+    int16_t year;
+    int8_t month, day, hour, min, sec;
+    parse_time(zda_msg, &year, &month, &day, &hour, &min, &sec);
 
     printf("ID: %s\n", zda_msg[0]);
     printf("hour: %d\n", hour);
     printf("min: %d\n", min);
     printf("sec: %d\n", sec);
-    printf("day: %d\n", atoi(zda_msg[2]));
-    printf("month: %d\n", atoi(zda_msg[3]));
-    printf("year: %d\n", atoi(zda_msg[4]));
+    printf("day: %d\n", day);
+    printf("month: %d\n", month);
+    printf("year: %d\n", year);
 }
