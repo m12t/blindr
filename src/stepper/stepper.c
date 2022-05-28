fc2013cd84a78b88583340b50b646ea86aa2fb5e
Michael Willett <mikerwillett@gmail.com>
2022-05-27 22:33:56 -0500

    removed calls to wake_stepper() and sleep_stepper() within the single_step() function to reduce stress on the stepper driver. the respective wake and sleep calls must be made around the loop that calls single_step()

--- src/stepper/stepper.c
+++ src/stepper/stepper.c
@@ -22,25 +22,24 @@ void wake_stepper() {
 
 void sleep_stepper() {
     gpio_put(SLEEP_PIN, 0);
-    sleep_ms(1);  // purely a safety margin
+    sleep_ms(2);  // purely a safety margin
 }
 
 int single_step(uint direction) {
-    // take a single step in the given direction
-    wake_stepper();
+    // take a single step in the given direction. This is used within a loop
+    // when using the 3 position toggle switch to "show" the program the blinds boundaries.
+    // NOTE: This function doesn't automatically wake and sleep the stepper!!!
+    //       this is done on purpose since this loop will run thousands of times when
+    //       finding the boundaries. Sleeping and waking the driver board 1000 times will place
+    //       unnecessary stress on the components. Just be sure to call wake_stepper() before
+    //       calling this function and sleep_stepper() after. Later when blinds movement are
+    //       automated, step_to() will be used and that handles waking and sleeping internally.
 
-    if (gpio_get_out_level(DIRECTION_PIN) != direction) {
-        // apparently gpio_get_out_level() should NOT be used like this
-        // according to https://raspberrypi.github.io/pico-sdk-doxygen/group__hardware__gpio.html#ga0a818ceaa50e3e2317fbb0856d47eaef
-        // set the stepper going in the correct direction
-        gpio_put(DIRECTION_PIN, direction);
-    }
+    gpio_put(DIRECTION_PIN, direction);  // the recommended solution is just to write this each
     // a single step is a rising edge. Do this by setting low then setting high
     gpio_put(STEP_PIN, 0);
     sleep_us(30);  // give a healthy margin between signals
     gpio_put(STEP_PIN, 1);
-
-    sleep_stepper();
 
     return 0;  // a step was completed normally
 }
@@ -50,8 +49,6 @@ int step_to(uint *current_position, uint desired_position, uint BOUNDARY_HIGH) {
     // and step to the desired destination, updating the current position pointer along the way
     // return 0 for no action taken, 1 for step(s) were performed. It's not necessary to return the number
     // or net number of steps taken as *current_position is updated each step here.
-
-    wake_stepper();
 
     // check that the inputs are valid
     if ((desired_position < 0) ||
